9ff5da471b6664cb91782f05855b86f8658d9151
蒲强 <pu.qiang@qq.com>
2021-10-18 18:02:07 +0800

    请求日志中间件

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/miaogu-go/Gof/core"
+	"github.com/miaogu-go/Gof/middlewares"
+	"io/ioutil"
 )
 
 func main() {
 	core.Bootstrap()
 	r := gin.New()
-	// 访问/version的返回值会随配置文件的变化而变化
+	r.Use(middlewares.RequestLog())
 	r.GET("/version", func(c *gin.Context) {
-
+		body, err := ioutil.ReadAll(c.Request.Body)
+		fmt.Println(string(body), err)
 	})
 
 	err := r.Run(":8080")
---
+++ middlewares/request_logger.go
@@ -0,0 +1,46 @@
+package middlewares
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/gin-gonic/gin"
+	"github.com/miaogu-go/Gof/global"
+	"go.uber.org/zap"
+	"io/ioutil"
+)
+
+//RequestLog 请求日志
+func RequestLog() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		body, err := ioutil.ReadAll(ctx.Request.Body)
+		ctx.Request.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+		ctx.Next()
+
+		host := ctx.Request.Host
+		query := ctx.Request.URL.RawQuery
+		uri := ctx.Request.RequestURI
+		method := ctx.Request.Method
+		proto := ctx.Request.Proto
+		ip := ctx.ClientIP()
+		path := ctx.Request.URL.Path
+		if err != nil {
+			global.GofLog.Warn("get request body failed,err:", zap.Error(err))
+		}
+		bodyMap := make(map[string]interface{})
+		err = json.Unmarshal(body, &bodyMap)
+		if err != nil {
+			global.GofLog.Warn("body Unmarshal failed,err:", zap.Error(err))
+		}
+		global.GofLog.Info("request log",
+			zap.String("host", host),
+			zap.String("uri", uri),
+			zap.String("method", method),
+			zap.String("query", query),
+			zap.String("path", path),
+			zap.String("proto", proto),
+			zap.Any("head", ctx.Request.Header),
+			zap.String("ip", ip),
+			zap.Any("body", bodyMap),
+		)
+	}
+}
