00f1ba046890a225db149f8d9f39b272f1e2f04c
蒲强 <pu.qiang@qq.com>
2021-10-19 18:00:52 +0800

    mongo 增加用户名与密码

--- config/mongodb.go
+++ config/mongodb.go
@@ -1,6 +1,8 @@
 package config
 
 type Mongodb struct {
-	Host string `json:"host" toml:"host" mapstructure:"host"`
-	Port int    `json:"port" toml:"port" mapstructure:"port"`
+	Host     string `json:"host" toml:"host" mapstructure:"host"`
+	Port     int    `json:"port" toml:"port" mapstructure:"port"`
+	Username string `json:"username" toml:"username" mapstructure:"username"`
+	Password string `json:"password" toml:"password" mapstructure:"password"`
 }
--- core/initialize/mongodb.go
+++ core/initialize/mongodb.go
@@ -12,8 +12,15 @@ import (
 //LoadMongodb 加载mongo
 func LoadMongodb() {
 	mongoConf := global.GofConfig.Mongodb
-	uri := fmt.Sprintf("mongodb://%s:%d", mongoConf.Host, mongoConf.Port)
-	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(uri))
+	//mongodb://username:password@host:%d
+	connStr := ""
+	if mongoConf.Username != "" && mongoConf.Password != "" {
+		connStr = fmt.Sprintf("mongodb://%s:%s@%s:%d", mongoConf.Username, mongoConf.Password, mongoConf.Host,
+			mongoConf.Port)
+	} else {
+		connStr = fmt.Sprintf("mongodb://%s:%d", mongoConf.Host, mongoConf.Port)
+	}
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(connStr))
 	if err != nil {
 		global.GofLog.Error("mongo connect failed,err:", zap.Error(err))
 		return
